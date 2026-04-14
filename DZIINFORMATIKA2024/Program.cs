2584acfe012302ff69a6984072523f650d605dce
stivan301 <spapanov@gmail.com>
2026-04-14 14:07:51 +0300

    Final

--- DZIINFORMATIKA2024/Program.cs
+++ DZIINFORMATIKA2024/Program.cs
@@ -13,19 +13,46 @@
                 List<string> lines = readFile.Split(Environment.NewLine).ToList();
                 //Обхождаме всички редове на файла
                 foreach (string line in lines) 
-                { 
-                   List<string>command=line
-                        .Split(" ",StringSplitOptions.RemoveEmptyEntries).ToList();
-                    if (command[0]== "enrollment")
+                {
+                    try
                     {
-                        var firstName = command[1];
-                        var lastName = command[2];
-                        var id= command[3];
-                        int age=int.Parse(command[4]);
-                        var parentLastName=command[5];
-                        var parentGsm=command[6];
-                        Kid kid = new Kid(firstName, lastName, id, age, parentLastName, parentGsm);
-                        Console.WriteLine(kinderGarden.EnrollKid(kid));
+                        List<string> command = line
+                        .Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+                        if (command[0] == "enrollment")
+                        {
+                            var firstName = command[1];
+                            var lastName = command[2];
+                            var id = command[3];
+                            int age = int.Parse(command[4]);
+                            var parentLastName = command[5];
+                            var parentGsm = command[6];
+                            Kid kid = new Kid(firstName, lastName, id, age, parentLastName, parentGsm);
+                            Console.WriteLine(kinderGarden.EnrollKid(kid));
+                        }
+                        else if (command[0] == "unsubscribe")
+                        {
+                            string id = command[1];
+                            Console.WriteLine(kinderGarden.ReleaseKid(id));
+                        }
+                        else if (command[0] == "information")
+                        {
+                            string group = command[1];
+                            Console.WriteLine(kinderGarden.GroupInfo(group));
+                        }
+                        else if (command[0] == "END")
+                        {
+                            Console.WriteLine("Have a nice day!");
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{command[0]} - invalid command.");
+                        }
+                    }
+                    catch (Exception e)
+                    {
+
+                        Console.WriteLine(e.Message);
                     }
 
                 }
