7899cc00e6c8fdf60f72eeb0fdcbdab385c8f33e
stivan301 <spapanov@gmail.com>
2026-04-14 13:53:31 +0300

    Program.cs relise

--- DZIINFORMATIKA2024/KinderGarden.cs
+++ DZIINFORMATIKA2024/KinderGarden.cs
@@ -11,5 +11,34 @@ namespace DZIINFORMATIKA2024
         {
             this.kidList = new List<Kid>();
         }
+        //Methods of class
+        public string EnrollKid(Kid kid)
+        {
+            kidList.Add(kid);
+            return $"The child {kid.FirstName} {kid.LastName} is enrolled.";
+        }//end 1
+        public string ReleaseKid(string id) 
+        {
+            Kid? searchKid=kidList.FirstOrDefault(k => k.Id == id);
+            if (searchKid != null) 
+            { 
+                kidList.Remove(searchKid);
+                return $"The child {searchKid.FirstName} {searchKid.LastName} has been unsubscribed.";
+            }
+            return $"Unsubscribe failed - invalid identifier {id}.";
+        }//end2
+        public string GroupInfo(string group)
+        {
+            StringBuilder sb = new StringBuilder();
+            int groupCount=kidList.Count(x=> x.Group == group);
+            string groupInfo = $"{group} group - {groupCount} children.";
+            sb.AppendLine(groupInfo);
+            var kidsGroup=kidList.Where(x=> x.Group == group);
+            foreach (var kid in kidsGroup) 
+            {
+                sb.AppendLine(kid.ToString());
+            }
+            return sb.ToString().Trim();
+        }//end3
     }
 }
--- DZIINFORMATIKA2024/Program.cs
+++ DZIINFORMATIKA2024/Program.cs
@@ -1,12 +1,34 @@
 ﻿namespace DZIINFORMATIKA2024
 {
+    using System.IO;
     public class Program
     {
         static void Main(string[] args)
         {
 			try
 			{
-               
+                KinderGarden kinderGarden=new KinderGarden();
+                StreamReader r = new StreamReader("data.txt");
+                string readFile=r.ReadToEnd();
+                List<string> lines = readFile.Split(Environment.NewLine).ToList();
+                //Обхождаме всички редове на файла
+                foreach (string line in lines) 
+                { 
+                   List<string>command=line
+                        .Split(" ",StringSplitOptions.RemoveEmptyEntries).ToList();
+                    if (command[0]== "enrollment")
+                    {
+                        var firstName = command[1];
+                        var lastName = command[2];
+                        var id= command[3];
+                        int age=int.Parse(command[4]);
+                        var parentLastName=command[5];
+                        var parentGsm=command[6];
+                        Kid kid = new Kid(firstName, lastName, id, age, parentLastName, parentGsm);
+                        Console.WriteLine(kinderGarden.EnrollKid(kid));
+                    }
+
+                }
 			}
 			catch (Exception ex)
 			{
